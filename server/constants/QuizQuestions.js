1dc0eb3314944ecf7397ecb87dab7210d9edda47
Tijmen <tijmen.coenders@vodafoneziggo.com>
2024-12-11 19:25:19 +0100

    added a "couple" questions

--- server/constants/QuizQuestions.js
+++ server/constants/QuizQuestions.js
@@ -1,50 +1,662 @@
 export const QUIZ_QUESTIONS = [
-    {
-      id: 'music-1',
-      text: 'Which band has the most platinum records?',
-      options: [
-        'The Beatles',
-        'Led Zeppelin',
-        'Pink Floyd',
-        'Eagles'
-      ],
-      correctAnswer: 0,
-      category: 'music'
-    },
-    {
-      id: 'music-2',
-      text: 'Who wrote the song "Imagine"?',
-      options: [
-        'Paul McCartney',
-        'John Lennon',
-        'George Harrison',
-        'Ringo Starr'
-      ],
-      correctAnswer: 1,
-      category: 'music'
-    },
-    {
-      id: 'games-1',
-      text: 'Which video game console has sold the most units worldwide?',
-      options: [
-        'PlayStation 2',
-        'Nintendo DS',
-        'Xbox 360',
-        'Nintendo Wii'
-      ],
-      correctAnswer: 0,
-      category: 'games'
-    },
-    {
-      id: 'movies-1',
-      text: 'Which movie won the Academy Award for Best Picture in 2020?',
-      options: [
-        '1917',
-        'Joker',
-        'Parasite',
-        'Once Upon a Time in Hollywood'
-      ],
-      correctAnswer: 2,
-      category: 'movies'
-    }
-  ];
+  {
+    id: 'drinks-1',
+    text: 'Which cocktail is made with rum, lime, and mint?',
+    options: [
+      'Margarita',
+      'Mojito',
+      'Cosmopolitan',
+      'Martini'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'trivia-1',
+    text: 'What is the most common color of toilet paper in France?',
+    options: [
+      'White',
+      'Pink',
+      'Blue',
+      'Yellow'
+    ],
+    correctAnswer: 1,
+    category: 'trivia'
+  },
+  {
+    id: 'drinks-2',
+    text: 'What is the main ingredient in a classic Bloody Mary?',
+    options: [
+      'Whiskey',
+      'Vodka',
+      'Gin',
+      'Tequila'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'fun-1',
+    text: 'Which animal is known to laugh when tickled?',
+    options: [
+      'Dog',
+      'Rat',
+      'Cat',
+      'Monkey'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'games-2',
+    text: 'In Mario Kart, what item can make all other racers spin out?',
+    options: [
+      'Banana',
+      'Green Shell',
+      'Blue Shell',
+      'Thunderbolt'
+    ],
+    correctAnswer: 2,
+    category: 'games'
+  },
+  {
+    id: 'drinks-3',
+    text: 'Which of these beers is originally from Ireland?',
+    options: [
+      'Heineken',
+      'Guinness',
+      'Corona',
+      'Budweiser'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'music-3',
+    text: 'Which pop star is known as "The Material Girl"?',
+    options: [
+      'Madonna',
+      'Britney Spears',
+      'Lady Gaga',
+      'Kylie Minogue'
+    ],
+    correctAnswer: 0,
+    category: 'music'
+  },
+  {
+    id: 'movies-2',
+    text: 'In the movie "The Hangover", what kind of animal is found in the bathroom?',
+    options: [
+      'Lion',
+      'Tiger',
+      'Monkey',
+      'Elephant'
+    ],
+    correctAnswer: 1,
+    category: 'movies'
+  },
+  {
+    id: 'drinks-4',
+    text: 'What is the term for a champagne and orange juice cocktail?',
+    options: [
+      'Bellini',
+      'Mimosa',
+      'Sangria',
+      'Spritz'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'fun-2',
+    text: 'What is illegal to do in Texas while standing up?',
+    options: [
+      'Drink beer',
+      'Play cards',
+      'Swear',
+      'Shoot a gun'
+    ],
+    correctAnswer: 0,
+    category: 'fun'
+  },
+  {
+    id: 'drinks-5',
+    text: 'Which country is the largest consumer of beer per capita?',
+    options: [
+      'United States',
+      'Germany',
+      'Czech Republic',
+      'Belgium'
+    ],
+    correctAnswer: 2,
+    category: 'drinks'
+  },
+  {
+    id: 'fun-3',
+    text: 'What is banned in public places in Florida after 6 PM on a Thursday?',
+    options: [
+      'Dancing',
+      'Singing',
+      'Farting',
+      'Eating peanuts'
+    ],
+    correctAnswer: 2,
+    category: 'fun'
+  },
+  {
+    id: 'movies-3',
+    text: 'In which movie does a character famously say, "You can’t handle the truth!"?',
+    options: [
+      'The Godfather',
+      'A Few Good Men',
+      'Goodfellas',
+      'Pulp Fiction'
+    ],
+    correctAnswer: 1,
+    category: 'movies'
+  },
+  {
+    id: 'music-4',
+    text: 'Which singer holds the record for the most Grammy wins?',
+    options: [
+      'Beyoncé',
+      'Michael Jackson',
+      'Taylor Swift',
+      'Stevie Wonder'
+    ],
+    correctAnswer: 0,
+    category: 'music'
+  },
+  {
+    id: 'drinks-6',
+    text: 'What type of alcohol is traditionally used in a Margarita?',
+    options: [
+      'Tequila',
+      'Vodka',
+      'Rum',
+      'Gin'
+    ],
+    correctAnswer: 0,
+    category: 'drinks'
+  },
+  {
+    id: 'trivia-2',
+    text: 'What is the collective noun for a group of flamingos?',
+    options: [
+      'Herd',
+      'Flock',
+      'Stand',
+      'Flamboyance'
+    ],
+    correctAnswer: 3,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-4',
+    text: 'Which U.S. state has a town named "Boring"?',
+    options: [
+      'California',
+      'Oregon',
+      'Texas',
+      'Nevada'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'games-3',
+    text: 'What is the best-selling video game of all time?',
+    options: [
+      'Minecraft',
+      'Grand Theft Auto V',
+      'Tetris',
+      'Call of Duty: Modern Warfare'
+    ],
+    correctAnswer: 0,
+    category: 'games'
+  },
+  {
+    id: 'fun-5',
+    text: 'What does the “D” in D-Day stand for?',
+    options: [
+      'Destiny',
+      'Deployment',
+      'Decision',
+      'Day'
+    ],
+    correctAnswer: 3,
+    category: 'fun'
+  },
+  {
+    id: 'drinks-7',
+    text: 'Which liquor is made from juniper berries?',
+    options: [
+      'Vodka',
+      'Whiskey',
+      'Gin',
+      'Tequila'
+    ],
+    correctAnswer: 2,
+    category: 'drinks'
+  },
+  {
+    id: 'trivia-3',
+    text: 'What is the only mammal capable of true flight?',
+    options: [
+      'Flying squirrel',
+      'Bat',
+      'Sugar glider',
+      'Gliding possum'
+    ],
+    correctAnswer: 1,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-6',
+    text: 'In Alaska, it is illegal to wake up which animal for the purpose of taking a photograph?',
+    options: [
+      'Bear',
+      'Moose',
+      'Wolf',
+      'Caribou'
+    ],
+    correctAnswer: 0,
+    category: 'fun'
+  },
+  {
+    id: 'drinks-8',
+    text: 'What is the term for a shot of whiskey dropped into a glass of beer?',
+    options: [
+      'Jägerbomb',
+      'Irish Car Bomb',
+      'Boilermaker',
+      'Black Velvet'
+    ],
+    correctAnswer: 2,
+    category: 'drinks'
+  },
+  {
+    id: 'drinks-9',
+    text: 'What is the main ingredient in a Piña Colada?',
+    options: [
+      'Pineapple juice',
+      'Orange juice',
+      'Cranberry juice',
+      'Lemonade'
+    ],
+    correctAnswer: 0,
+    category: 'drinks'
+  },
+  {
+    id: 'fun-7',
+    text: 'In which country is it illegal to chew gum in public?',
+    options: [
+      'United States',
+      'Singapore',
+      'India',
+      'Australia'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'trivia-4',
+    text: 'What is the tiny piece of plastic at the end of a shoelace called?',
+    options: [
+      'Aglet',
+      'Spindle',
+      'Tassel',
+      'Caplet'
+    ],
+    correctAnswer: 0,
+    category: 'trivia'
+  },
+  {
+    id: 'movies-4',
+    text: 'Which movie features the line, "May the Force be with you"?',
+    options: [
+      'Star Wars',
+      'Star Trek',
+      'Guardians of the Galaxy',
+      'Dune'
+    ],
+    correctAnswer: 0,
+    category: 'movies'
+  },
+  {
+    id: 'music-5',
+    text: 'Who is often referred to as the "King of Pop"?',
+    options: [
+      'Elvis Presley',
+      'Prince',
+      'Michael Jackson',
+      'Freddie Mercury'
+    ],
+    correctAnswer: 2,
+    category: 'music'
+  },
+  {
+    id: 'drinks-10',
+    text: 'What is the alcohol content of the standard proof in the United States?',
+    options: [
+      '40%',
+      '50%',
+      '60%',
+      '70%'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'games-4',
+    text: 'What is the name of the protagonist in the Legend of Zelda series?',
+    options: [
+      'Zelda',
+      'Link',
+      'Ganondorf',
+      'Epona'
+    ],
+    correctAnswer: 1,
+    category: 'games'
+  },
+  {
+    id: 'trivia-5',
+    text: 'What is the official national animal of Scotland?',
+    options: [
+      'Deer',
+      'Unicorn',
+      'Lion',
+      'Eagle'
+    ],
+    correctAnswer: 1,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-8',
+    text: 'Which body part is known as the “funny bone”?',
+    options: [
+      'Knee',
+      'Elbow',
+      'Ankle',
+      'Wrist'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'movies-5',
+    text: 'In the movie "Titanic," what is the name of the ship’s captain?',
+    options: [
+      'Captain Smith',
+      'Captain Phillips',
+      'Captain Cook',
+      'Captain Jones'
+    ],
+    correctAnswer: 0,
+    category: 'movies'
+  },
+  {
+    id: 'music-6',
+    text: 'Which classical composer was deaf for most of his life?',
+    options: [
+      'Mozart',
+      'Beethoven',
+      'Bach',
+      'Chopin'
+    ],
+    correctAnswer: 1,
+    category: 'music'
+  },
+  {
+    id: 'drinks-11',
+    text: 'What is the traditional garnish for a martini?',
+    options: [
+      'Cherry',
+      'Olive',
+      'Lime wedge',
+      'Orange peel'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'games-5',
+    text: 'Which video game franchise includes a character named Master Chief?',
+    options: [
+      'Call of Duty',
+      'Halo',
+      'Destiny',
+      'Battlefield'
+    ],
+    correctAnswer: 1,
+    category: 'games'
+  },
+  {
+    id: 'trivia-6',
+    text: 'What is the longest-running animated TV show in the United States?',
+    options: [
+      'South Park',
+      'The Simpsons',
+      'Family Guy',
+      'SpongeBob SquarePants'
+    ],
+    correctAnswer: 1,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-9',
+    text: 'What type of fish is Nemo in the movie "Finding Nemo"?',
+    options: [
+      'Goldfish',
+      'Clownfish',
+      'Betta Fish',
+      'Guppy'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'movies-6',
+    text: 'In "Jurassic Park," what kind of dinosaur is the first one seen by the characters?',
+    options: [
+      'Tyrannosaurus Rex',
+      'Triceratops',
+      'Brachiosaurus',
+      'Velociraptor'
+    ],
+    correctAnswer: 2,
+    category: 'movies'
+  },
+  {
+    id: 'music-7',
+    text: 'Which artist is famous for the album "Thriller"?',
+    options: [
+      'Prince',
+      'Michael Jackson',
+      'Whitney Houston',
+      'Madonna'
+    ],
+    correctAnswer: 1,
+    category: 'music'
+  },
+  {
+    id: 'drinks-12',
+    text: 'Which country is famous for producing Cognac?',
+    options: [
+      'Italy',
+      'France',
+      'Spain',
+      'Germany'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'trivia-7',
+    text: 'What is the rarest M&M color?',
+    options: [
+      'Blue',
+      'Green',
+      'Brown',
+      'Yellow'
+    ],
+    correctAnswer: 2,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-10',
+    text: 'What color are aircraft black boxes?',
+    options: [
+      'Black',
+      'Orange',
+      'Yellow',
+      'White'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'movies-7',
+    text: 'Who directed the movie "Inception"?',
+    options: [
+      'Christopher Nolan',
+      'Steven Spielberg',
+      'Quentin Tarantino',
+      'James Cameron'
+    ],
+    correctAnswer: 0,
+    category: 'movies'
+  },
+  {
+    id: 'music-8',
+    text: 'What is Elvis Presley’s middle name?',
+    options: [
+      'Aron',
+      'Aaron',
+      'Earl',
+      'James'
+    ],
+    correctAnswer: 1,
+    category: 'music'
+  },
+  {
+    id: 'drinks-13',
+    text: 'What is the traditional base spirit for a Mojito?',
+    options: [
+      'Vodka',
+      'Rum',
+      'Tequila',
+      'Gin'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'games-6',
+    text: 'Which game features the characters Mario, Luigi, and Bowser?',
+    options: [
+      'Sonic the Hedgehog',
+      'Donkey Kong',
+      'Super Mario Bros',
+      'Kirby'
+    ],
+    correctAnswer: 2,
+    category: 'games'
+  },
+  {
+    id: 'trivia-8',
+    text: 'How many bones are there in the adult human body?',
+    options: [
+      '206',
+      '208',
+      '210',
+      '212'
+    ],
+    correctAnswer: 0,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-11',
+    text: 'What does the acronym "YOLO" stand for?',
+    options: [
+      'You Only Love Once',
+      'You Only Live Once',
+      'Your Own Lovely Owl',
+      'You Only Laugh Occasionally'
+    ],
+    correctAnswer: 1,
+    category: 'fun'
+  },
+  {
+    id: 'movies-8',
+    text: 'Who played Jack in the movie "Titanic"?',
+    options: [
+      'Brad Pitt',
+      'Johnny Depp',
+      'Leonardo DiCaprio',
+      'Matt Damon'
+    ],
+    correctAnswer: 2,
+    category: 'movies'
+  },
+  {
+    id: 'music-9',
+    text: 'Which band was Freddie Mercury the lead singer of?',
+    options: [
+      'Queen',
+      'The Beatles',
+      'The Rolling Stones',
+      'Pink Floyd'
+    ],
+    correctAnswer: 0,
+    category: 'music'
+  },
+  {
+    id: 'drinks-14',
+    text: 'What is the origin country of tequila?',
+    options: [
+      'Brazil',
+      'Mexico',
+      'Spain',
+      'Peru'
+    ],
+    correctAnswer: 1,
+    category: 'drinks'
+  },
+  {
+    id: 'games-7',
+    text: 'What does "RPG" stand for in gaming?',
+    options: [
+      'Role-Playing Game',
+      'Random Player Game',
+      'Rapid Play Game',
+      'Realistic Player Game'
+    ],
+    correctAnswer: 0,
+    category: 'games'
+  },
+  {
+    id: 'trivia-9',
+    text: 'What is the capital city of Australia?',
+    options: [
+      'Sydney',
+      'Melbourne',
+      'Canberra',
+      'Brisbane'
+    ],
+    correctAnswer: 2,
+    category: 'trivia'
+  },
+  {
+    id: 'fun-12',
+    text: 'Which element is represented by the symbol "O" on the periodic table?',
+    options: [
+      'Oxygen',
+      'Osmium',
+      'Oxide',
+      'Oganesson'
+    ],
+    correctAnswer: 0,
+    category: 'fun'
+  }
+];
